62869958938eca003322dc885468290723e08de6
Yu Liang <yliang62@gmail.com>
2018-09-18 13:25:01 -0700

    final version2

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,35 +1,41 @@
-public class Calculator{
-    Calculator(){
+public class Calculator {
+    Calculator() {
 
     }
-    public  int add(int a , int b){
+
+    public int add(int a, int b) {
         return a + b;
     }
-    public  int subtract(int a , int b){
+
+    public int subtract(int a, int b) {
         return a - b;
     }
-    public  int multiply(int a , int b){
-        return a * b;}
-    public int divide(int a , int b){
-        if (b == 0){
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
             System.out.println("Error! Dividing by zero is not allowed.");
             return 0;
-        }else{
-            return a/b;
+        } else {
+            return a / b;
         }
     }
-    public int modulo(int a, int b){
-        if (b == 0){
+
+    public int modulo(int a, int b) {
+        if (b == 0) {
             System.out.println("Error! Dividing by zero is not allowed.");
             return 0;
-        }else{
-            return a%b;
+        } else {
+            return a % b;
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
         System.out.println(myCalculator.add(5, 7));
         System.out.println(myCalculator.subtract(45, 11));
-
     }
 }
