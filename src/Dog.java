23a88736363b01cf182eb3b5253fba93b6c9b299
Yu Liang <yliang62@gmail.com>
2018-09-18 14:09:58 -0700

    init

---
+++ src/Dog.java
@@ -0,0 +1,26 @@
+
+class Dog{
+    int age;
+    public Dog(int dogsAge){
+        age = dogsAge;
+    }
+
+    public void bark(){
+        System.out.println("Woof!");
+    }
+    public void run(int feet){
+        System.out.println("Your dog ran " + feet + "feet!");
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public static void main(String[] args) {
+        Dog spike = new Dog(2);
+        spike.bark();
+        spike.run(10);
+        int spikeAge = spike.getAge();
+        System.out.println(spikeAge);
+
+    }
+}
