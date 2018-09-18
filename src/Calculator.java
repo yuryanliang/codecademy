09158a76cd89c738a4de5aef191471847d457d08
Yu Liang <yliang62@gmail.com>
2018-09-18 13:25:50 -0700

    final version2

--- src/Calculator.java
+++ src/Calculator.java
@@ -33,6 +33,7 @@ public class Calculator {
         }
     }
 
+
     public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
         System.out.println(myCalculator.add(5, 7));
