a87563b38f416c122d94e4fa13354cfc89a25d4e
Yu Liang <yliang62@gmail.com>
2018-09-18 16:10:38 -0700

    init

---
+++ src/Library.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class Library {
+    public Library(){
+
+    }
+
+    public void getFinishedBooks(HashMap<String, Boolean> library){
+        if (library.size() < 1){
+            System.out.println("Library is empty!");
+
+        }else {
+            for (String book: library.keySet()){
+                if (library.get(book) == true){
+                   System.out.println(book);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        HashMap<String, Boolean> myBooks = new HashMap<String, Boolean>();
+        myBooks.put("Road Down The Funnel", true);
+        myBooks.put("Rat: A Biology", false);
+        myBooks.put("TimeIn", true);
+        myBooks.put("3D Food Printing", false);
+
+        Library myLibrary = new Library();
+        myLibrary.getFinishedBooks(myBooks);
+    }
+
+}
